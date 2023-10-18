d6a6ca65ceb32bc8335d3eb895214003533ebcd0
wantanwonderland <shafuan1015@gmail.com>
2023-10-19 01:49:37 +0800

    update plugin

--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -8,14 +8,25 @@ import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
 import { Autosave } from '@ckeditor/ckeditor5-autosave';
-import { Bold, Code, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import {
+	Bold,
+	Code,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline
+} from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
 import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Heading, Title } from '@ckeditor/ckeditor5-heading';
 import { Highlight } from '@ckeditor/ckeditor5-highlight';
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 import {
 	AutoImage,
 	Image,
@@ -27,20 +38,27 @@ import {
 	ImageUpload
 } from '@ckeditor/ckeditor5-image';
 import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
-import { TextPartLanguage } from '@ckeditor/ckeditor5-language';
 import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { DocumentList } from '@ckeditor/ckeditor5-list';
+import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
+import { Markdown } from '@ckeditor/ckeditor5-markdown-gfm';
 import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
 import { Mention } from '@ckeditor/ckeditor5-mention';
 import { PageBreak } from '@ckeditor/ckeditor5-page-break';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { SelectAll } from '@ckeditor/ckeditor5-select-all';
+import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
 import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
 import {
 	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
 	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
 	SpecialCharactersText
 } from '@ckeditor/ckeditor5-special-characters';
+import { Style } from '@ckeditor/ckeditor5-style';
 import {
 	Table,
 	TableCaption,
@@ -67,15 +85,17 @@ class Editor extends ClassicEditor {
 		Bold,
 		Code,
 		CodeBlock,
-		DocumentList,
 		Essentials,
+		FindAndReplace,
 		FontBackgroundColor,
 		FontColor,
 		FontFamily,
 		FontSize,
+		GeneralHtmlSupport,
 		Heading,
 		Highlight,
 		HorizontalLine,
+		HtmlEmbed,
 		Image,
 		ImageCaption,
 		ImageInsert,
@@ -88,25 +108,40 @@ class Editor extends ClassicEditor {
 		Italic,
 		Link,
 		LinkImage,
+		List,
+		ListProperties,
+		Markdown,
 		MediaEmbed,
 		MediaEmbedToolbar,
 		Mention,
 		PageBreak,
 		Paragraph,
 		RemoveFormat,
+		SelectAll,
+		ShowBlocks,
 		SimpleUploadAdapter,
 		SourceEditing,
 		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
 		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
 		SpecialCharactersText,
+		Strikethrough,
+		Style,
+		Subscript,
+		Superscript,
 		Table,
 		TableCaption,
 		TableCellProperties,
 		TableColumnResize,
 		TableProperties,
 		TableToolbar,
-		TextPartLanguage,
 		TextTransformation,
+		Title,
+		TodoList,
+		Underline,
 		WordCount
 	];
 
@@ -118,8 +153,6 @@ class Editor extends ClassicEditor {
 				'bold',
 				'italic',
 				'link',
-				'highlight',
-				'alignment',
 				'bulletedList',
 				'numberedList',
 				'|',
@@ -127,21 +160,35 @@ class Editor extends ClassicEditor {
 				'indent',
 				'|',
 				'imageUpload',
-				'imageInsert',
 				'blockQuote',
 				'insertTable',
 				'mediaEmbed',
 				'undo',
 				'redo',
-				'code',
-				'pageBreak',
+				'findAndReplace',
+				'codeBlock',
 				'fontBackgroundColor',
 				'fontColor',
 				'fontFamily',
 				'fontSize',
+				'htmlEmbed',
 				'horizontalLine',
+				'imageInsert',
+				'pageBreak',
 				'removeFormat',
-				'sourceEditing'
+				'showBlocks',
+				'selectAll',
+				'sourceEditing',
+				'specialCharacters',
+				'strikethrough',
+				'superscript',
+				'subscript',
+				'style',
+				'underline',
+				'highlight',
+				'code',
+				'todoList',
+				'alignment'
 			]
 		},
 		language: 'en',
